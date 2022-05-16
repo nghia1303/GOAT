48810716ecd2692110610b43da7209b4087440e9
Pham Trong Nghia (Tristan) <phamnghia974@gmail.com>
2022-05-16 21:31:56 +0700

    Nhung file da them sua

---
+++ effect.js
@@ -0,0 +1,11 @@
+// gsap.registerPlugin(ScrollTrigger);
+// gsap.to(".welcome", {
+//     yPercent: -100,
+//     ease: "none",
+//     scrollTrigger: {
+//       trigger: ".container-fluid trigger",
+//       // start: "top bottom", // the default values
+//       // end: "bottom top",
+//       scrub: true
+//     }, 
+//   });
